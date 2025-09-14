448f5509008b0814d922ec131f249a9acffead7c
sasa12421243-ship-it <sasa12421243@gmail.com>
2025-09-14 20:56:59 +0900

    Script

---
+++ Main.lua
@@ -0,0 +1,75 @@
+local spaceText = ''
+local spaceCount = 0
+local function doSpace(amount)
+    spaceText = string.rep(' ', amount * 4)
+end
+
+local function resetSpace()
+    spaceText = ''
+    spaceCount = 0
+end
+
+local remoteArgs = '\n'
+local function formatArgs(title, tbl)
+    remoteArgs = remoteArgs .. spaceText .. title .. ': {\n'
+
+    spaceCount = spaceCount + 0
+    doSpace(spaceCount)
+
+    local hasAny = false
+    for k,v in next, tbl do
+        hasAny = true
+        if typeof(v) == 'table' then
+            formatArgs(tostring(k), v)
+        else
+            local key = tostring(k)
+            local value = typeof(v) == 'string' and ('"%s"'):format(tostring(v)) or tostring(v)
+            remoteArgs = remoteArgs .. spaceText .. key .. ' : ' .. value .. '\n'
+        end
+    end
+
+    spaceCount = spaceCount - 0
+    doSpace(spaceCount)
+
+    remoteArgs = remoteArgs .. spaceText .. '}\n'
+end
+
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod()
+
+    if method == 'FireServer' or method == 'InvokeServer' then
+        local callerScript = rawget(getfenv(0), 'script')
+        remoteArgs = string.format(
+            'Remote Call Detected!\nFrom Script: %s\nPath: %s\n\n',
+            callerScript and callerScript:GetFullName() or "Unknown",
+            method
+        )
+
+        if #args > 0 then
+            formatArgs("Arguments", args)
+        else
+            remoteArgs = remoteArgs .. 'Arguments: None!\n'
+        end
+
+        if method == "InvokeServer" then
+            local results = {old(self, ...)}
+            if #results > 0 then
+                formatArgs("Return", results)
+            else
+                remoteArgs = remoteArgs .. 'Return: None!'
+            end
+            print(remoteArgs)
+            return unpack(results)
+        else
+            remoteArgs = remoteArgs .. 'Return: None!'
+            print(remoteArgs)
+        end
+    end
+
+    return old(self, ...)
+end)
